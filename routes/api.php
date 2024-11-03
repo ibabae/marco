6dae4f84cfffbef4d4295662fa15c713e74b4ff6
Saman Babaei <alibabaeian670@gmail.com>
2024-11-03 15:08:10 +0330

    remove api auth

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('login',AuthController::class);
-Route::middleware(['auth:api'])->group(function () {
-    Route::prefix('admin')->group(function(){
-        Route::apiResource('user',UserController::class);
-    });
-});
