eb09621b12aa9a2be4731960a8f5bda8dddde909
Saman Babaei <alibabaeian670@gmail.com>
2024-11-04 12:18:52 +0330

    add swagger details

--- app/Http/Controllers/Admin/UserManagement/PermissionController.php
+++ app/Http/Controllers/Admin/UserManagement/PermissionController.php
@@ -17,41 +17,129 @@ class PermissionController extends Controller
     ){}
 
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *      path="/api/admin/user-management/permission",
+     *      summary="Permissions data",
+     *      tags={"Permissions"},
+     *      description="list of permissions",
+     *      @OA\Response(
+     *          response=200,
+     *          description="list",
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="unauthorized",
+     *      )
+     * )
      */
-    public function index()
+
+     public function index()
     {
         return $this->service->allPermissions();
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *      path="/api/admin/user-management/permission",
+     *      summary="Create Permission",
+     *      tags={"Permissions"},
+     *      description="create Permission",
+     *      @OA\Parameter(
+     *          name="name",
+     *          in="query",
+     *          description="Permission name",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="unique"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful created",
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="unauthorized",
+     *      )
+     * )
      */
-    public function store(StorePermissionRequest $request)
+
+     public function store(StorePermissionRequest $request)
     {
         return $this->service->createPermission($request->validated());
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *      path="/api/admin/user-management/permission/{id}",
+     *      summary="Permissions data",
+     *      tags={"Permissions"},
+     *      description="show a permission",
+     *      @OA\Response(
+     *          response=200,
+     *          description="show item",
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="unauthorized",
+     *      )
+     * )
      */
-    public function show(string $id)
+
+     public function show(string $id)
     {
         return $this->service->findPermission($id);
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *      path="/api/admin/user-management/permission/{id}",
+     *      summary="Update Permission",
+     *      tags={"Permissions"},
+     *      description="update Permission",
+     *      @OA\Parameter(
+     *          name="name",
+     *          in="query",
+     *          description="Permission name",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="unique"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful updated",
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="unauthorized",
+     *      )
+     * )
      */
-    public function update(UpdatePermissionRequest $request, string $id)
+
+     public function update(UpdatePermissionRequest $request, string $id)
     {
         return $this->service->updatePermission($request->validated(), $id);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *      path="/api/admin/user-management/permission/{id}",
+     *      summary="Permissions data",
+     *      tags={"Permissions"},
+     *      description="delete a permission",
+     *      @OA\Response(
+     *          response=200,
+     *          description="permission deleted",
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="unauthorized",
+     *      )
+     * )
      */
-    public function destroy(string $id)
+
+     public function destroy(string $id)
     {
         return $this->service->deletePermission($id);
     }
