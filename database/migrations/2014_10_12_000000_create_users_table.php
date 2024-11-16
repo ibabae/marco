7241ab45d92b6a71e813dca000e7094f18c9ec22
Saman Babaei <alibabaeian670@gmail.com>
2024-11-16 10:12:52 +0330

    change first and last name to snake case

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName')->nullable();
-            $table->string('lastName')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->bigInteger('phone')->unique();
             $table->string('email')->nullable()->unique();
             $table->string('password');
