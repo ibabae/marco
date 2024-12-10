39c766d2413aaa453d84a69bfc432400c665069d
Saman Babaei <alibabaeian670@gmail.com>
2024-12-10 21:15:12 +0330

    add migrations

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class MenuItem extends Model
 {
+    protected $keyType = 'string';
+    public $incrementing = false;
+    public static function booted() {
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+
     protected $guarded = [];
 
     public $timestamps = false;
---
+++ database/migrations/2024_11_12_140017_create_category_attributes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('parent_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('attribute_id')->constrained()->onDelete('cascade');
+            $table->integer('priority')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_attributes');
+    }
+};
